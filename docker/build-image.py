9976e5da56a38fcee0ae75e241b9601d7fc164b8
jamefrank <13121950875@163.com>
2023-11-03 02:00:36 +0800

    to finish docker

---
+++ docker/build-image.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import docker
+from setuptools_scm import get_version
+
+CURRENT_DIR = Path(__file__).parent
+CURRENT_DIR = str(CURRENT_DIR)
+
+NAME = "tdx-tools"
+VERSION = get_version(root='..', relative_to=__file__).split('+')[0]
+IMAGE = f"{NAME}:{VERSION}"
+
+
+def main():
+    print(f"Building image {IMAGE}")
+    client = docker.from_env()
+    build_output = client.api.build(path=CURRENT_DIR, dockerfile='Dockerfile', tag=IMAGE)
+    for line in build_output:
+        if 'stream' in line.decode('utf-8'):
+            print(line.decode('utf-8'), end='')
+
+
+if __name__ == '__main__':
+    main()
