b6c6b4fe7f9642e738f7f8e31856b5570495c4ce
fanjin <>
2023-11-03 10:49:05 +0800

    finish build docker image and test success

--- docker/build-image.py
+++ docker/build-image.py
@@ -2,8 +2,10 @@ from pathlib import Path
 import docker
 from setuptools_scm import get_version
 
-CURRENT_DIR = Path(__file__).parent
+CURRENT_DIR = Path(__file__).parent.parent
 CURRENT_DIR = str(CURRENT_DIR)
+
+print("current dir:", CURRENT_DIR)
 
 NAME = "tdx-tools"
 VERSION = get_version(root='..', relative_to=__file__).split('+')[0]
@@ -13,7 +15,7 @@ IMAGE = f"{NAME}:{VERSION}"
 def main():
     print(f"Building image {IMAGE}")
     client = docker.from_env()
-    build_output = client.api.build(path=CURRENT_DIR, dockerfile='Dockerfile', tag=IMAGE)
+    build_output = client.api.build(path=CURRENT_DIR, dockerfile='docker/Dockerfile', tag=IMAGE, rm=True)
     for line in build_output:
         if 'stream' in line.decode('utf-8'):
             print(line.decode('utf-8'), end='')
