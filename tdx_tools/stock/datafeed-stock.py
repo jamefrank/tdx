3f2d06ce3fced1651390d2ddaaffb5f60d89aee3
范瑾 <jin.fan@novauto.com.cn>
2023-11-11 10:23:18 +0800

    update stock list to mongodb

---
+++ tdx_tools/stock/datafeed-stock.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+# @File    :   datafeed-stock.py
+# @Time    :   2023/11/11 10:21:54
+# @Author  :   frank
+# @Email:
+# @Description: 从第三方库获取股票列表的信息，并更新到数据库
--- tdx_tools/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-sh_stock_list = api.get_security_list(MarkeType.shanghai.value, 0)
-# sh_stock_list = api.get_security_list(MarkeType.shenzhen.value, 0)
-print(len(sh_stock_list))
