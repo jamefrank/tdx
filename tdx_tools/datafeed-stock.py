76b97893777186c6e9ec70af0b007bce21f1a0f8
fanjin <>
2023-11-11 17:01:01 +0800

    add mongodb

--- tdx_tools/datafeed-stock.py
+++ tdx_tools/datafeed-stock.py
@@ -6,3 +6,15 @@
 # @Description: 从第三方库获取股票列表的信息，并更新到数据库
 
 import akshare as ak
+import tdx_tools.stock.list_pb2 as stock_list
+
+
+def get_stock_list():
+    """获取股票列表
+    """
+    stock_list = ak.stock_info_sh_name_code()
+    print(stock_list)
+
+
+if __name__ == '__main__':
+    get_stock_list()
